5a1a72b4c0bcf59aacf993d6edb0e4452b3780ba
glebka <b.gleba77@gmail.com>
2026-03-24 21:14:04 +0300

    user info ready

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router'
-import avatar from '../assets/img/500x500.jpg'
+import avatar from '../assets/img/real_avatar.jpg'
 
 const UserProfile = () => {
+    const firstName = 'Александр'
+    const secondName = 'Петров'
+    const email = 'b.gleba77@gmail.com'
+    const createdAt = '24 марта 2026 г.'
+
     return (
-        <div className="userProfile">
-            <h1>Профиль</h1>
-            <img src={avatar} alt='Avatar' />
-        </div>
-    )
-}
+        <div className="userProfile__card">
+            <img className="userProfile__avatar" src={avatar} alt="Фото профиля" />
+            <div className="userProfile__info">
+                <div class="userProfile__fullName">
+                    {`${firstName} ${secondName}`}
+                </div>
+                <div className="userProfile__row">
+                    <div className="userProfile__label">Почта:</div>
+                    <span>{email}</span>
+                </div>
+                <div className="userProfile__row">
+                    <div className="userProfile__label">Дата регистрации:</div>
+                    <span>{createdAt}</span>
+                </div>
+            </div>
+        </div >
+    );
+};
 
 export default UserProfile
