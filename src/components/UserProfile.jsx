6eb7aae5978483f3f40b440336ab36921e78366f
glebka <b.gleba77@gmail.com>
2026-03-25 07:57:11 +0300

    user profile section

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import baseAvatar from '../assets/img/real_avatar.jpg'
+import baseAvatar from '../assets/img/500x500.jpg'
 import { useContext } from 'react';
 import { UserInfoContext } from '../context/UserInfo/UserInfoContext';
 import { formatDate } from '../utils/dataFormatter';
@@ -13,22 +13,42 @@ const UserProfile = () => {
     } = useContext(UserInfoContext)
 
     return (
-        <div className="userProfile__card">
-            <img className="userProfile__avatar" src={avatar !== '' ? avatar : baseAvatar} alt="Фото профиля" />
-            <div className="userProfile__info">
-                <div className="userProfile__fullName">
-                    {`${firstName} ${secondName}`}
-                </div>
-                <div className="userProfile__row">
-                    <div className="userProfile__label">Почта:</div>
-                    <span>{email}</span>
-                </div>
-                <div className="userProfile__row">
-                    <div className="userProfile__label">Дата регистрации:</div>
-                    <span>{formatDate(createdAt)}</span>
+        <>
+            <div className="userProfile__section">
+                <div className='userProfile__card'>
+                    <div className='userProfile__avatar-wrapper'>
+                        <img
+                            className='userProfile__avatar'
+                            src={avatar || baseAvatar}
+                            alt='Фото профиля'
+                        />
+                        <div className='userProfile__avatar-overlay'>
+                            <span>Изменить фото</span>
+                        </div>
+                    </div>
+                    <div className='userProfile__info'>
+                        <div className='userProfile__fullName'>
+                            {`${firstName} ${secondName}`}
+                        </div>
+                        <div className='userProfile__row'>
+                            <div className='userProfile__label'>Почта:</div>
+                            <span>{email}</span>
+                        </div>
+                        <div className='userProfile__row'>
+                            <div className='userProfile__label'>Дата регистрации:</div>
+                            <span>{formatDate(createdAt)}</span>
+                        </div>
+                    </div>
+                </div >
+            </div>
+            <div className='userProfile__section'>
+                <div className='userProfile__h1'>Команды</div>
+                <div className='userProfile__row team'>
+                    <div>Команда1</div>
+                    <div>Администратор</div>
                 </div>
             </div>
-        </div >
+        </>
     );
 };
 
