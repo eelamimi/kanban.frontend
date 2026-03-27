d8fa4432522b47694f9d4179198f42c2c5d98066
glebka <b.gleba77@gmail.com>
2026-03-27 17:58:37 +0300

    user profile spinner in center

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,12 +1,19 @@
 import UserProfileCard from './UserProfile/Card'
 import Section from './Section'
 import TeamsSection from './TeamsSection'
+import Spinner from './Spinner'
+import { useContext } from 'react';
+import { UserInfoContext } from '../context/UserInfo/UserInfoContext';
 
 const UserProfile = () => {
+    const {
+        isUserInfoLoading
+    } = useContext(UserInfoContext)
+
     return (
         <>
             <Section>
-                <UserProfileCard />
+                {isUserInfoLoading ? <Spinner /> : <UserProfileCard />}
             </Section>
             <TeamsSection />
         </>
--- src/components/UserProfile/Card.jsx
+++ src/components/UserProfile/Card.jsx
@@ -5,7 +5,6 @@ import UserProfileAvatar from './Avatar'
 import UserProfileInfo from './Info'
 import UserProfileFullName from './FullName'
 import UserProfileField from './Field'
-import Spinner from '../Spinner';
 
 const UserProfileCard = () => {
     const {
@@ -13,33 +12,26 @@ const UserProfileCard = () => {
         secondName,
         email,
         avatar,
-        isUserInfoLoading,
         createdAt,
     } = useContext(UserInfoContext)
 
     return (
         <div className='userProfile__card'>
-            {isUserInfoLoading ? (
-                <Spinner />
-            ) : (
-                <>
-                    <UserProfileAvatar avatar={avatar} />
-                    <UserProfileInfo>
-                        <UserProfileFullName
-                            firstName={firstName}
-                            secondName={secondName}
-                        />
-                        <UserProfileField
-                            label='Почта'
-                            value={email}
-                        />
-                        <UserProfileField
-                            label='Зарегистрирован'
-                            value={formatDate(createdAt)}
-                        />
-                    </UserProfileInfo>
-                </>
-            )}
+            <UserProfileAvatar avatar={avatar} />
+            <UserProfileInfo>
+                <UserProfileFullName
+                    firstName={firstName}
+                    secondName={secondName}
+                />
+                <UserProfileField
+                    label='Почта'
+                    value={email}
+                />
+                <UserProfileField
+                    label='Зарегистрирован'
+                    value={formatDate(createdAt)}
+                />
+            </UserProfileInfo>
         </div >
     )
 }
