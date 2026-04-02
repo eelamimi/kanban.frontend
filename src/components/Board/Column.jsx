36a47d7fc937b4031579bfb19085a8cfed77dfcd
glebka <b.gleba77@gmail.com>
2026-04-02 08:51:09 +0300

    some shit

--- src/components/Board/Column.jsx
+++ src/components/Board/Column.jsx
@@ -2,13 +2,21 @@ import Section from '../Section'
 import Issue from './Issue'
 
 const Column = ({ column }) => {
+    const issues = [
+        { id: 1, title: 'title1' },
+        { id: 2, title: 'title2' },
+        { id: 3, title: 'title3' },
+        { id: 4, title: 'title4' },
+        { id: 5, title: 'title5' },
+        { id: 6, title: 'title6' },
+    ]
     return (
         <div className="column">
             <Section className='columnName'>
                 {column.name}
             </Section>
             <Section className='issues'>
-                {column.issues.map((issue) => (
+                {issues.map((issue) => (
                     <Issue issue={issue} key={issue.id} />
                 ))}
             </Section>
--- src/components/Board/Columns.jsx
+++ src/components/Board/Columns.jsx
@@ -11,15 +11,6 @@ const Columns = ({ columns }) => {
             </div>
             <div className="columns">
                 {columns.map((column) => {
-                    column.issues = [
-                        { id: 1, title: 'title1' },
-                        { id: 2, title: 'title2' },
-                        { id: 3, title: 'title3' },
-                        { id: 4, title: 'title4' },
-                        { id: 5, title: 'title5' },
-                        { id: 6, title: 'title6' },
-                    ]
-
                     return (
                         <Column column={column} key={column.id} />
                     )
