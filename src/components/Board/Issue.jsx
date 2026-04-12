5289afbfd9bc0838a1a9fad91580ed2754a4e0de
glebka <b.gleba77@gmail.com>
2026-04-12 15:47:28 +0300

    issue images

--- src/components/Board/Issue.jsx
+++ src/components/Board/Issue.jsx
@@ -1,11 +1,11 @@
 import { useDraggable } from '@dnd-kit/core'
 import { memo, useContext, useMemo } from 'react'
 import { ProjectContext } from '../../context/Project/ProjectContext'
-import minimalImg from '../../assets/img/issuePriority/500x500.jpg'
-import lowImg from '../../assets/img/issuePriority/500x500.jpg'
-import mediumImg from '../../assets/img/issuePriority/500x500.jpg'
-import highImg from '../../assets/img/issuePriority/500x500.jpg'
-import criticalImg from '../../assets/img/issuePriority/500x500.jpg'
+import lowestImg from '../../assets/img/issuePriority/lowest.png'
+import lowImg from '../../assets/img/issuePriority/low.png'
+import mediumImg from '../../assets/img/issuePriority/medium.png'
+import highImg from '../../assets/img/issuePriority/high.png'
+import highestImg from '../../assets/img/issuePriority/highest.png'
 import bugImg from '../../assets/img/issueType/bug.png'
 import taskImg from '../../assets/img/issueType/task.png'
 import storyImg from '../../assets/img/issueType/story.png'
@@ -19,11 +19,11 @@ const issueTypesImg = {
 }
 
 const issuePriorityImg = {
-    0: minimalImg,
+    0: lowestImg,
     1: lowImg,
     2: mediumImg,
     3: highImg,
-    4: criticalImg,
+    4: highestImg,
 }
 
 const Issue = ({ issue }) => {
