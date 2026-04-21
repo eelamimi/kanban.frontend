84a239fd38a1d82eaa3246f05c0201da3ed31430
glebka <b.gleba77@gmail.com>
2026-04-21 21:47:34 +0300

    start to design issue details

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,8 +1,21 @@
+import { useContext } from 'react'
+import IssueHeader from './IssueDetails/IssueHeader'
 import Spinner from './Spinner'
+import { IssueContext } from '../context/Issue/IssueContext'
+import Section from './Section'
 
 const IssueDetails = () => {
+    const { isLoadingIssue } = useContext(IssueContext)
+
+    if (isLoadingIssue)
+        return (
+            <Section>
+                <Spinner />
+            </Section>
+        )
+
     return (
-        <Spinner />
+        <IssueHeader />
     )
 }
 
--- src/components/IssueDetails/IssueHeader.jsx
+++ src/components/IssueDetails/IssueHeader.jsx
@@ -1,5 +1,17 @@
-const IssueHeader = () => {
+import { useContext } from 'react'
+import { IssueContext } from '../../context/Issue/IssueContext'
+import Section from '../Section'
 
+const IssueHeader = () => {
+    const { issue } = useContext(IssueContext)
+
+    console.log(issue)
+
+    return (
+        <Section>
+            <div className="h1">{issue.title}</div>
+        </Section>
+    )
 }
 
 export default IssueHeader
