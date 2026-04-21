d4005ad82dfbf3d2ffb5fd2d87e764c8616fb479
glebka <b.gleba77@gmail.com>
2026-04-21 21:55:38 +0300

    start to do issue detail

--- src/components/IssueDetails/IssueHeader.jsx
+++ src/components/IssueDetails/IssueHeader.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { IssueContext } from '../../context/Issue/IssueContext'
 import Section from '../Section'
+import Button from '../Button'
 
 const IssueHeader = () => {
     const { issue } = useContext(IssueContext)
@@ -8,8 +9,26 @@ const IssueHeader = () => {
     console.log(issue)
 
     return (
-        <Section>
-            <div className="h1">{issue.title}</div>
+        <Section className='eight'>
+            <div className='subsection'>
+                <div className="h1">{issue.title}</div>
+                <Button
+                    className='left'
+                // TODO
+                //
+                // onClick={() => setIsEditBoardOpen(true)}
+                >
+                    Редактировать
+                </Button>
+                {/*
+                TODO 
+                edit issue modal
+
+                <EditBoardModal
+                    isOpen={isEditBoardOpen}
+                    onClose={() => setIsEditBoardOpen(false)}
+                /> */}
+            </div>
         </Section>
     )
 }
