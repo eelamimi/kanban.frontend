91703d9602cb01cc9f3445b495974a91b534aaee
glebka <b.gleba77@gmail.com>
2026-04-22 08:45:05 +0300

    refactor: issue consts to file

--- src/components/Board/Issue.jsx
+++ src/components/Board/Issue.jsx
@@ -1,31 +1,8 @@
 import { useDraggable } from '@dnd-kit/core'
 import { memo, useContext, useMemo } from 'react'
 import { ProjectContext } from '../../context/Project/ProjectContext'
-import lowestImg from '../../assets/img/issuePriority/lowest.png'
-import lowImg from '../../assets/img/issuePriority/low.png'
-import mediumImg from '../../assets/img/issuePriority/medium.png'
-import highImg from '../../assets/img/issuePriority/high.png'
-import highestImg from '../../assets/img/issuePriority/highest.png'
-import bugImg from '../../assets/img/issueType/bug.png'
-import taskImg from '../../assets/img/issueType/task.png'
-import storyImg from '../../assets/img/issueType/story.png'
-import investigationImg from '../../assets/img/issueType/investigation.png'
 import { Link } from 'react-router'
-
-const issueTypesImg = {
-    0: bugImg,
-    1: storyImg,
-    2: taskImg,
-    3: investigationImg,
-}
-
-const issuePriorityImg = {
-    0: lowestImg,
-    1: lowImg,
-    2: mediumImg,
-    3: highImg,
-    4: highestImg,
-}
+import { issuePriorityImg, issueTypesImg } from '../../consts/issueConsts'
 
 const Issue = ({ issue }) => {
     const { project } = useContext(ProjectContext)
---
+++ src/consts/issueConsts.js
@@ -0,0 +1,24 @@
+import lowestImg from '../assets/img/issuePriority/lowest.png'
+import lowImg from '../assets/img/issuePriority/low.png'
+import mediumImg from '../assets/img/issuePriority/medium.png'
+import highImg from '../assets/img/issuePriority/high.png'
+import highestImg from '../assets/img/issuePriority/highest.png'
+import bugImg from '../assets/img/issueType/bug.png'
+import taskImg from '../assets/img/issueType/task.png'
+import storyImg from '../assets/img/issueType/story.png'
+import investigationImg from '../assets/img/issueType/investigation.png'
+
+export const issueTypesImg = {
+    0: bugImg,
+    1: storyImg,
+    2: taskImg,
+    3: investigationImg,
+}
+
+export const issuePriorityImg = {
+    0: lowestImg,
+    1: lowImg,
+    2: mediumImg,
+    3: highImg,
+    4: highestImg,
+}
