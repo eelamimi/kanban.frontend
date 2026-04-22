318202f20e0e76d8556e0baa4062384d94a4d1d4
glebka <b.gleba77@gmail.com>
2026-04-22 09:30:04 +0300

    description of issue

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -3,6 +3,7 @@ import IssueHeader from './IssueDetails/IssueHeader'
 import Spinner from './Spinner'
 import { IssueContext } from '../context/Issue/IssueContext'
 import Section from './Section'
+import IssueDescription from './IssueDetails/IssueCommentary'
 
 const IssueDetails = () => {
     const { isLoadingIssue } = useContext(IssueContext)
@@ -20,7 +21,10 @@ const IssueDetails = () => {
         )
 
     return (
-        <IssueHeader />
+        <>
+            <IssueHeader />
+            <IssueDescription />
+        </>
     )
 }
 
