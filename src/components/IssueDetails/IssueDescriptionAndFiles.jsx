48634fe5b2910f075a24bbb78b8bb0bd7ea2a777
glebka <b.gleba77@gmail.com>
2026-04-24 16:43:10 +0300

    download file

--- src/components/IssueDetails/IssueDescriptionAndFiles.jsx
+++ src/components/IssueDetails/IssueDescriptionAndFiles.jsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react'
+import { useCallback, useContext } from 'react'
+import { saveAs } from 'file-saver';
 import Section from '../Section'
 import { IssueContext } from '../../context/Issue/IssueContext'
 import Span from '../Span'
@@ -9,6 +10,21 @@ const IssueDescriptionAndFiles = () => {
     const { issue } = useContext(IssueContext)
     const description = issue.commentaries.find((com) => com.isDescription)
     const files = issue.attachments.length === 0 ? null : issue.attachments
+
+    const downloadFile = useCallback((file) => {
+        try {
+            const binaryString = atob(file.content)
+            const bytes = new Uint8Array(binaryString.length)
+            for (let i = 0; i < binaryString.length; i++) {
+                bytes[i] = binaryString.charCodeAt(i)
+            }
+            const blob = new Blob([bytes], { type: file.contentType })
+            saveAs(blob, file.fileName)
+        } catch (error) {
+            console.error('Download error:', error)
+            alert('Не удалось загрузить файл')
+        }
+    }, [])
 
     return (
         <Section className='eight'>
@@ -30,9 +46,17 @@ const IssueDescriptionAndFiles = () => {
                         <div className='h1'>Файлы</div>
                         <div className="issue-files__files">
                             {files.map(file => (
-                                <span key={file.id} className='issue-files__file'>
+                                <a
+                                    className='issue-files__file'
+                                    key={file.id}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        downloadFile(file);
+                                    }}
+                                    href="#"
+                                >
                                     {file.fileName} ({formatFileSize(file.size)})
-                                </span>
+                                </a>
                             ))}
                         </div>
                     </div>}
