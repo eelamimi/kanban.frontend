39c05e38119d3588d2d321d6ff06825e21ad6091
glebka <b.gleba77@gmail.com>
2026-04-24 15:43:40 +0300

    commentary component

--- src/components/IssueDetails/IssueCommentary.jsx
+++ src/components/IssueDetails/IssueCommentary.jsx
@@ -1,6 +1,41 @@
+import baseAvatar from '../../assets/img/default_avatar.jpg'
+import AuthService from '../../service/AuthService'
+import { formatDate } from '../../utils/dataFormatter'
+
+
 const IssueCommentary = ({ commentary }) => {
+    const { author } = commentary
+    const fullName = `${author.firstName} ${author.secondName}`
+    const isAuthor = author.id === AuthService.getUserInfo().userProfileId
+
     return (
-        <span>{commentary.content}</span>
+        <div className='commentary__container'>
+            <div className='commentary__header'>
+                <img
+                    className='commentary__avatar'
+                    src={author.avatar || baseAvatar}
+                    alt={fullName}
+                    title={`Фото профиля ${fullName}`}
+                    width='20px'
+                    height='20px'
+                    loading='lazy'
+                />
+                <div className='commentary__fullName'>
+                    {fullName}
+                </div>
+                {commentary.isEdited &&
+                    <div className='commentary__edited'>
+                        {`Изменено: ${formatDate(commentary.lastEditedAt)}`}
+                    </div>}
+            </div>
+            <div className='commentary__content'>
+                {commentary.content}
+            </div>
+            {isAuthor &&
+                <button className='commentary__editButton'>
+                    Редактировать
+                </button>}
+        </div>
     )
 }
 
