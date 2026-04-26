f8d5788ad6bb746b679cfb5c58400457c1a3aa36
glebka <b.gleba77@gmail.com>
2026-04-26 15:03:56 +0300

    feat(todo): edit and delete commentary

--- src/components/IssueDetails/IssueCommentary.jsx
+++ src/components/IssueDetails/IssueCommentary.jsx
@@ -1,14 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import baseAvatar from '../../assets/img/default_avatar.jpg'
 import AuthService from '../../service/AuthService'
 import { formatDate } from '../../utils/dataFormatter'
 import Button from '../Button'
 import Span from '../Span'
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons'
+import { useCallback, useEffect } from 'react'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import TextAreaField from '../TextAreaField'
+import { useEditCommentary } from '../../hook/useEditCommentary'
 
 
 const IssueCommentary = ({ commentary }) => {
+    const editInnerHandler = useCallback(async () => {
+
+    }, [])
+
+    const {
+        content,
+        error,
+        setContent,
+        onContentInput,
+        isEditingCommentary,
+        isEditing,
+        isDeleting,
+        handleEditCommentary,
+        handleDeleteCommentary
+    } = useEditCommentary(editInnerHandler)
     const { author } = commentary
     const fullName = `${author.firstName} ${author.secondName}`
     const isAuthor = author.id === AuthService.getUserInfo().userProfileId
+
+    useEffect(() => {
+        setContent(commentary.content)
+    }, [commentary.content, setContent])
 
     return (
         <div className='commentary__container'>
@@ -33,15 +58,34 @@ const IssueCommentary = ({ commentary }) => {
                     />}
             </div>
             <div className='commentary__content'>
-                {commentary.content}
+                {isEditingCommentary
+                    ? <TextAreaField
+                        id={`editCommentaryContent${commentary.id}`}
+                        fieldClassName='full-width no-margin-bottom'
+                        textareaClassName='full-width no-margin-bottom'
+                        value={content}
+                        onInput={onContentInput}
+                        error={error}
+                    />
+                    : commentary.content}
             </div>
             {isAuthor &&
                 <div className='commentary_buttons'>
-                    <Button className='commentary__editButton'>
-                        Редактировать
+                    <Button
+                        className='commentary__editButton'
+                        onClick={handleEditCommentary}
+                        isDisabled={isEditing}
+                    >
+                        {isEditingCommentary
+                            ? <FontAwesomeIcon icon={faCheck} />
+                            : <FontAwesomeIcon icon={faPenToSquare} />}
                     </Button>
-                    <Button className='commentary__deleteButton close'>
-                        Удалить
+                    <Button
+                        className='commentary__deleteButton close'
+                        onClick={handleDeleteCommentary}
+                        isDisabled={isDeleting}
+                    >
+                        <FontAwesomeIcon icon={faX} />
                     </Button>
                 </div>}
         </div>
