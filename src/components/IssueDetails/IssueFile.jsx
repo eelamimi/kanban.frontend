7e9225a47f39916c0fa2b90f082fb67bdb3e6a19
glebka <b.gleba77@gmail.com>
2026-04-26 11:28:28 +0300

    use attachmentAPI

--- src/components/IssueDetails/IssueFile.jsx
+++ src/components/IssueDetails/IssueFile.jsx
@@ -1,11 +1,18 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { saveAs } from 'file-saver';
 import { formatFileSize } from '../../utils/fileFormatter';
+import attachmentAPI from '../../api/attachmentAPI';
+import { showError } from '../../utils/errorHandler';
 
 const IssueFile = ({ file }) => {
+    const [isLoading, setIsLoading] = useState(false)
     const downloadFile = useCallback((file) => {
         try {
-            const binaryString = atob(file.content)
+            setIsLoading(true)
+
+            const response = attachmentAPI.get({ id: file.id })
+
+            const binaryString = atob(response)
             const bytes = new Uint8Array(binaryString.length)
             for (let i = 0; i < binaryString.length; i++) {
                 bytes[i] = binaryString.charCodeAt(i)
@@ -13,22 +20,26 @@ const IssueFile = ({ file }) => {
             const blob = new Blob([bytes], { type: file.contentType })
             saveAs(blob, file.fileName)
         } catch (error) {
-            console.error('Download error:', error)
-            alert('Не удалось загрузить файл')
+            showError(error)
+            // console.error('Download error:', error)
+            // alert('Не удалось загрузить файл')
+        } finally {
+            setIsLoading(false)
         }
     }, [])
 
     return (
         <a
             className='issue-files__file'
-            key={file.id}
             onClick={(e) => {
                 e.preventDefault();
                 downloadFile(file);
             }}
             href="#"
         >
-            {file.fileName} ({formatFileSize(file.size)})
+            {isLoading
+                ? 'Загрузка...'
+                : `${file.fileName} ${formatFileSize(file.size)}`}
         </a>
     )
 }
