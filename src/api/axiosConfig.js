d2d1069191f9c24c43ffe543fcb3c053e441a431
glebka <b.gleba77@gmail.com>
2026-04-26 11:40:43 +0300

    401 show error message

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -43,7 +43,7 @@ axiosInstance.interceptors.response.use(
                 showError(message)
         }
         if (error.response?.status === 401) {
-            console.log('un authorize')
+            showError('Вы не авторизованы')
             AuthService.clearUserInfo()
             AuthService.redirectToLogin()
         }
