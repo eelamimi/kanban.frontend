7cdb47a3f3214bc8c82fc4264feb4d84f4f0ac57
glebka <b.gleba77@gmail.com>
2026-04-27 19:58:42 +0300

    set edit issue modal values by default

--- src/components/Modals/EditIssueModal.jsx
+++ src/components/Modals/EditIssueModal.jsx
@@ -1,8 +1,26 @@
-import { memo, useCallback, useState } from 'react'
+import { memo, useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import Modal from '../Modal'
+import { ProjectContext } from '../../context/Project/ProjectContext'
+import { IssueContext } from '../../context/Issue/IssueContext'
+import { useEditIssueModal } from '../../hook/useEditIssueModal'
 
 const EditIssueModal = ({ isOpen, onClose }) => {
+    const { memberIdOptions } = useContext(ProjectContext)
+    const { issue } = useContext(IssueContext)
     const [isWaiting, setIsWaiting] = useState(false)
+    const {
+        resetValues
+    } = useEditIssueModal()
+
+    const assigneeOption = useMemo(() => {
+        if (!issue?.assignee.id) return null
+        memberIdOptions.find(member => member.value === issue.assignee.id)
+    }, [issue.assignee.id, memberIdOptions])
+
+    const authorOption = useMemo(() => {
+        if (!issue?.author.id) return null
+        memberIdOptions.find(member => member.value === issue.author.id)
+    }, [issue.author.id, memberIdOptions])
 
     const editIssue = useCallback(async () => {
         setIsWaiting(true)
@@ -12,6 +30,12 @@ const EditIssueModal = ({ isOpen, onClose }) => {
     const handleClose = useCallback(() => {
         onClose()
     }, [onClose])
+
+    useEffect(() => {
+        if (issue === null || assigneeOption === null || authorOption === null)
+            return
+        resetValues(issue, assigneeOption, authorOption)
+    }, [assigneeOption, authorOption, issue, resetValues])
 
     return (
         <Modal
