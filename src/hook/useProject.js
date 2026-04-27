28057bc354e098bdeac20dcd45737e25ed1dad46
glebka <b.gleba77@gmail.com>
2026-04-27 19:33:41 +0300

    fix rerenders

--- src/hook/useProject.js
+++ src/hook/useProject.js
@@ -1,5 +1,5 @@
 import baseAvatar from '../assets/img/default_avatar.jpg'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { useParams, useSearchParams } from 'react-router'
 import projectAPI from '../api/projectAPI'
 import { ProjectContext } from '../context/Project/ProjectContext'
@@ -13,21 +13,27 @@ export const useProject = () => {
     const projectIdFromUrl = searchParams.get('projectId')
     const [isLoadingProject, setIsLoadingProject] = useState(true)
     const [project, setProject] = useState(null)
-    const [curUser, setCurUser] = useState(null)
-    const [memberIdOptions, setMemberIdOptions] = useState([])
+
+    const memberIdOptions = useMemo(() => {
+        if (!project?.members) return []
+        return project.members.map((member) => ({
+            value: member.id,
+            label: `${member.firstName} ${member.secondName}`,
+            img: member.avatar === "" ? baseAvatar : member.avatar,
+            imgClassName: 'member-avatar-option'
+        }))
+    }, [project?.members])
+
+    const curUser = useMemo(() => {
+        if (!memberIdOptions.length) return null
+        return memberIdOptions.find(member => member.value === userProfileId)
+    }, [memberIdOptions])
 
     useEffect(() => {
         async function fetchProject(projectId) {
             try {
                 const response = await projectAPI.get(projectId)
                 setProject(response)
-                setMemberIdOptions(project.members.map((member) => ({
-                    value: member.id,
-                    label: `${member.firstName} ${member.secondName}`,
-                    img: member.avatar === "" ? baseAvatar : member.avatar,
-                    imgClassName: 'member-avatar-option'
-                })) ?? [])
-                setCurUser(memberIdOptions.find(member => member.value === userProfileId))
             }
             catch (error) {
                 console.log(error)
@@ -38,7 +44,7 @@ export const useProject = () => {
         }
 
         fetchProject(projectId || projectIdFromUrl)
-    }, [project?.members, memberIdOptions, projectId, projectIdFromUrl])
+    }, [projectId, projectIdFromUrl])
 
     return {
         project,
