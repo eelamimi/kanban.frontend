6b909f7c985176ab19977e891afcee094d900802
glebka <b.gleba77@gmail.com>
2026-04-27 18:48:34 +0300

    start of edit issue modal

--- src/components/Modals/EditIssueModal.jsx
+++ src/components/Modals/EditIssueModal.jsx
@@ -1,9 +1,28 @@
-import { memo } from 'react'
+import { memo, useCallback, useState } from 'react'
+import Modal from '../Modal'
 
-const EditIssueModal = () => {
+const EditIssueModal = ({ isOpen, onClose }) => {
+    const [isWaiting, setIsWaiting] = useState(false)
+
+    const editIssue = useCallback(async () => {
+        setIsWaiting(true)
+        setIsWaiting(false)
+    }, [])
+
+    const handleClose = useCallback(() => {
+        onClose()
+    }, [onClose])
+
     return (
-        <>
-        </>
+        <Modal
+            isOpen={isOpen}
+            title={'Изменить проблему'}
+            actionTitle={'Изменить'}
+            onAction={editIssue}
+            onClose={handleClose}
+            isDisabled={isWaiting}
+        >
+        </Modal>
     )
 }
 
