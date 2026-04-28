b2f00c0ceafa3c8d946a770162497e47c343667c
glebka <b.gleba77@gmail.com>
2026-04-28 08:20:36 +0300

    format code

--- src/components/UserProfile/Avatar.jsx
+++ src/components/UserProfile/Avatar.jsx
@@ -1,16 +1,18 @@
 import baseAvatar from '../../assets/img/default_avatar.jpg'
 
 const UserProfileAvatar = ({ avatar }) => {
-    return (<div className='userProfile__avatar-wrapper'>
-        <img
-            className='userProfile__avatar'
-            src={avatar || baseAvatar}
-            alt='Фото профиля'
-        />
-        <div className='userProfile__avatar-overlay'>
-            <span>Изменить фото</span>
+    return (
+        <div className='userProfile__avatar-wrapper'>
+            <img
+                className='userProfile__avatar'
+                src={avatar || baseAvatar}
+                alt='Фото профиля'
+            />
+            <div className='userProfile__avatar-overlay'>
+                <span>Изменить фото</span>
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default UserProfileAvatar
