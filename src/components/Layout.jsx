6b59df198e051884f29db8e3bd74f0f72f479bf8
glebka <b.gleba77@gmail.com>
2026-04-29 16:04:47 +0300

    delete header from layout to start from scratch

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,29 +1,12 @@
 // import exitIcon from '../assets/img/icon_exit.png'
 import { Outlet } from 'react-router'
-import NavItem from './NavItem'
-// import UserInfoProvider from '../context/UserInfo/UserInfoProvider'
 
 const Layout = () => {
 
 
     return (
         <>
-            <header>
-                <nav>
-                    <NavItem className='combobox'>Projects</NavItem>
-                    <NavItem className='combobox'>Projects</NavItem>
-                    <NavItem className='combobox'>Projects</NavItem>
-                    <NavItem className='combobox'>Projects</NavItem>
-                    <NavItem className='profile'>Profile</NavItem>
-                    {/* <NavItem className='profile-exit'>
-                        <img
-                            src={exitIcon}
-                            alt='exit'
-                        />
-                    </NavItem> */}
-                </nav>
-            </header>
-            <main>
+        <main>
                 <Outlet />
             </main>
         </>
