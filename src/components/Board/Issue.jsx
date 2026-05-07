e06fc28fe0c24f8912af91201b12043b8865f215
glebka <b.gleba77@gmail.com>
2026-05-07 15:33:29 +0300

    issue with assignee image

--- src/components/Board/Issue.jsx
+++ src/components/Board/Issue.jsx
@@ -3,13 +3,24 @@ import { memo, useContext, useMemo } from 'react'
 import { ProjectContext } from '../../context/Project/ProjectContext'
 import { Link } from 'react-router'
 import { issuePriorityImg, issueTypesImg } from '../../consts/issueConsts'
+import baseAvatar from '../../assets/img/default_avatar.jpg'
 
 const Issue = ({ issue }) => {
     const { project } = useContext(ProjectContext)
-    const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
-        id: issue.id,
-    })
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        isDragging
+    } = useDraggable({ id: issue.id, })
     const issuePublicId = `${project.shortName}-${issue.numberInProject}`
+    const avatar = useMemo(() => {
+        return issue.assignee.avatar
+    }, [issue.assignee.avatar])
+    const assigneeImgTitle = useMemo(() => {
+        return `Исполнитель: ${issue.assignee.firstName} ${issue.assignee.secondName}`
+    }, [])
 
     const style = useMemo(() => {
         return {
@@ -27,33 +38,46 @@ const Issue = ({ issue }) => {
             {...listeners}
             {...attributes}
         >
-            <div className='issue-row'>
-                <div className='issue_img-container'>
-                    <img
-                        src={issueTypesImg[issue.issueType]}
-                        width='14'
-                        height='14'
-                    />
+            <div className='row'>
+                <div className='column no-gap'>
+                    <div className='issue-row'>
+                        <div className='issue_img-container'>
+                            <img
+                                src={issueTypesImg[issue.issueType]}
+                                width='14'
+                                height='14'
+                            />
+                        </div>
+                        <Link
+                            className={`issue_publicId ${issue.isDeleted ? `deleted` : ``}`}
+                            to={`/issue/${issuePublicId}?projectId=${project.id}`}
+                        >
+                            {issuePublicId}
+                        </Link>
+                    </div>
+                    <div className='issue-row'>
+                        <div className='issue_title'>{issue.title}</div>
+                    </div>
+                    <div className='issue-row last'>
+                        <div className='issue_img-container'>
+                            <img
+                                src={issuePriorityImg[issue.issuePriority]}
+                                width='14'
+                                height='14'
+                            />
+                        </div>
+                        <div className='issue_storyPoints'>{issue.storyPoints}</div>
+                    </div>
                 </div>
-                <Link
-                    className={`issue_publicId ${issue.isDeleted ? `deleted` : ``}`}
-                    to={`/issue/${issuePublicId}?projectId=${project.id}`}
-                >
-                    {issuePublicId}
-                </Link>
-            </div>
-            <div className='issue-row'>
-                <div className='issue_title'>{issue.title}</div>
-            </div>
-            <div className='issue-row last'>
-                <div className='issue_img-container'>
-                    <img
-                        src={issuePriorityImg[issue.issuePriority]}
-                        width='14'
-                        height='14'
-                    />
-                </div>
-                <div className='issue_storyPoints'>{issue.storyPoints}</div>
+                <img
+                    className='issue-assignee-avatar'
+                    src={!avatar ? baseAvatar : `data:image/jpeg;base64,${avatar}`}
+                    alt={assigneeImgTitle}
+                    title={assigneeImgTitle}
+                    loading='lazy'
+                    width={32}
+                    height={32}
+                />
             </div>
         </div>
     )
